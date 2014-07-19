36b5bb09f8bb8f38c716c7c58b2b1ba7c21645e3
Anurag Banerjee <anuragb2010@gmail.com>
2014-07-18 23:54:27 -0400

    Truncatable primes
    The number 3797 has an interesting property. Being prime itself, it is
    possible to continuously remove digits from left to right, and remain
    prime at each stage: 3797, 797, 97, and 7. Similarly we can work from
    right to left: 3797, 379, 37, and 3.
    
    Find the sum of the only eleven primes that are both truncatable from
    left to right and right to left.
    
    NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.

---
+++ 037.py
@@ -0,0 +1,57 @@
+# answer is 748317
+# timed ~ 6.5 seconds
+
+def isprime(candidate):
+	if candidate == 2: return True
+	if candidate <= 1 or candidate % 2 == 0: return False
+	i = 3
+	while candidate/(i-1) > i:
+		if candidate % i == 0: return False
+		i += 2
+	return True
+
+def islistofprimes(list):
+	return all((isprime(x) for x in list))
+
+def getdigits(x):
+	digits = []
+	while x:
+		digits.insert(0, x % 10)
+		x /= 10
+	return digits
+
+def cutleft(x, digits):
+	output = []
+	l = len(digits)-1
+	while l:
+		shift = 10 ** l
+		x -= (digits[0] * shift)
+		l -= 1
+		output += [x]
+		del digits[0]
+	return output
+
+def cutright(x, digits):
+	if not digits: digits = digits(x)
+	output = []
+	while x / 10 > 0:
+	    output += [x / 10]
+	    x /= 10
+	return output
+
+def istruncatedprime(number):
+	digits = getdigits(number)
+	part1 = cutright(number, digits)
+	if not islistofprimes(part1): return False
+	part2 = cutleft(number, digits)
+	return islistofprimes(part1 + part2)
+
+def main():
+	truncatable_primes, current_number = [], 9
+	while len(truncatable_primes) < 11:
+		if not isprime(current_number): current_number += 2; continue
+		if istruncatedprime(current_number): truncatable_primes.append(current_number)
+		current_number += 2
+	return sum(truncatable_primes)
+
+print main()
