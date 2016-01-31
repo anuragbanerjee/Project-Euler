c0a85e5cd00aa0fedb634cba5b44489910670de3
Anurag Banerjee <anuragb2010@gmail.com>
2016-01-31 03:25:49 -0500

    Add Problem 18 and 67 (Maximum Path Sum) in Python

---
+++ 018.py
@@ -0,0 +1,59 @@
+# Problem 18
+# https://projecteuler.net/problem=18
+# Question: Find the maximum total from top to bottom of the triangle below:
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+# Answer: 1074
+#
+# Approach: Divide-and-conquer. Add up best possible score from bottom per row.
+#
+
+def main():
+  pyramid = [
+    [75],
+    [95, 64],
+    [17, 47, 82],
+    [18, 35, 87, 10],
+    [20,  4, 82, 47, 65],
+    [19,  1, 23, 75,  3, 34],
+    [88,  2, 77, 73,  7, 63, 67],
+    [99, 65,  4, 28,  6, 16, 70, 92],
+    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+    [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+    [04, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]
+  ][::-1]
+
+  answer = 0
+  for id, row in enumerate(pyramid):
+    score = reduce_pyramid(id, row, pyramid)
+    if score > answer: answer = score
+  print answer
+
+def reduce_pyramid(id, row, pyramid):
+  if id + 1 >= len(pyramid): return
+  pyr, i = pyramid[id + 1], 0
+  while len(row) > 1:
+    pyr[i] += max(row[0:2])
+    del row[0]
+    i += 1
+  return max(pyr)
+
+if __name__ == '__main__':
+  main()
